952bb46c0d9843d3c41befca0b7150422c77c88f
yuki <yuki.onishi1129@gmail.com>
2022-10-01 03:05:14 +0900

    add: fetch todo detail

--- web-front/src/apis/todoApi.ts
+++ web-front/src/apis/todoApi.ts
@@ -1,15 +1,34 @@
-import { AxiosResponse } from 'axios'
-import { globalAxios, isAxiosError } from '@/apis/config'
-import {TodoType} from "@/interfaces/Todo"
+import { AxiosResponse } from 'axios';
+import { globalAxios, isAxiosError } from '@/apis/config';
+import { TodoType } from '@/interfaces/Todo';
 
-
+/**
+ * fetchTodoListApi
+ * @returns
+ */
 export const fetchTodoListApi = async () => {
-    try {
-        const {data}:AxiosResponse<Array<TodoType>> = await globalAxios.get('todo')
-        return data
-    } catch(err) {
-        if (isAxiosError(err)) {
-            return err.code
-        }
+  try {
+    const { data }: AxiosResponse<Array<TodoType>> = await globalAxios.get('todo');
+    return data;
+  } catch (err) {
+    if (isAxiosError(err)) {
+      return err.code;
     }
-}
+  }
+};
+
+/**
+ * fetchTodoDetailApi
+ * @param id
+ * @returns
+ */
+export const fetchTodoDetailApi = async (id: number) => {
+  try {
+    const { data }: AxiosResponse<TodoType> = await globalAxios.get(`todo/${id}`);
+    return data;
+  } catch (err) {
+    if (isAxiosError(err)) {
+      return err.code;
+    }
+  }
+};
--- web-front/src/components/templates/TodoDetailTemplate/index.tsx
+++ web-front/src/components/templates/TodoDetailTemplate/index.tsx
@@ -3,22 +3,19 @@
  *
  * @package components
  */
-import { useRouter } from 'next/router';
-import { useTodoContext } from '@/contexts/TodoContext';
+import { FC } from 'react';
 import { BaseLayout } from '@/components/organisms/BaseLayout';
 import { InputForm } from '@/components/atoms/InputForm';
 import { TextArea } from '@/components/atoms/TextArea';
+import { useTodoDetailTemplate } from './useTodoDetailTemplate';
 import styles from './styles.module.css';
 
 /**
  * TodoDetailTemplate
- * @returns {JSX.Element}
- * @constructor
+ * @returns
  */
-export const TodoDetailTemplate = () => {
-  const { originTodoList } = useTodoContext();
-  const router = useRouter();
-  const todo = originTodoList.find((todo) => String(todo.id) === router?.query?.id);
+export const TodoDetailTemplate: FC = () => {
+  const [{ todo }] = useTodoDetailTemplate();
 
   return (
     <BaseLayout title={'TodoDetail'}>
