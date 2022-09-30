b04d506be7d62cb971fd0d3be76a9051e4927865
yuki <yuki.onishi1129@gmail.com>
2022-10-01 01:56:12 +0900

    add: todo find all

--- api-server/src/todo/todo.service.ts
+++ api-server/src/todo/todo.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { Todo } from '@prisma/client';
+import { PrismaService } from '../prisma.service';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
 
 @Injectable()
 export class TodoService {
+  constructor(private prisma: PrismaService) {}
+
   create(createTodoDto: CreateTodoDto) {
     return 'This action adds a new todo';
   }
 
-  findAll() {
-    return `This action returns all todo`;
+  async findAll(): Promise<Todo[]> {
+    return this.prisma.todo.findMany();
   }
 
   findOne(id: number) {
