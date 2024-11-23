5d95032f7b55c66cd618c2fafa2b4cbf06a21a72
daichi0918 <kayasugadaichi@icloud.com>
2024-11-23 14:01:46 +0900

    apiフォルダ作成

--- frontend/src/contexts/TodoContext.tsx
+++ frontend/src/contexts/TodoContext.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { fetchTodoListApi } from '@/apis/todoApi';
 // import { INITIAL_TODO_LIST, INITIAL_TODO_LIST_LENGTH } from '@/constants/data';
 import { TodoType } from '@/interfaces/Todo';
 import axios from 'axios';
@@ -7,6 +8,7 @@ import React, {
   FC,
   ReactNode,
   createContext,
+  useCallback,
   useEffect,
   useState,
 } from 'react';
@@ -24,28 +26,17 @@ interface TodoContextInterface {
 
 export const TodoContext = createContext({} as TodoContextInterface);
 
-export const fetchData = async (
-  setOriginalTodoList: Function,
-  setTodoListLength: Function
-) => {
-  try {
-    const response = await axios.get(
-      `${process.env.NEXT_PUBLIC_END_POINT}/api/todo`
-    );
-    const data = response.data;
-    setOriginalTodoList(data);
-    setTodoListLength(data.length);
-  } catch (error) {
-    console.error('Error fetching data:', error);
-  }
-};
-
 export const TodoProvider: FC<Props> = ({ children }) => {
   const [originalTodoList, setOriginalTodoList] = useState<Array<TodoType>>([]);
   const [todoListLength, setTodoListLength] = useState<number>(0);
 
+  const fetchData = useCallback(async () => {
+    const data = await fetchTodoListApi();
+    setOriginalTodoList(typeof data === 'object' ? data : []);
+  }, []);
+
   useEffect(() => {
-    fetchData(setOriginalTodoList, setTodoListLength);
+    fetchData();
   }, []);
   return (
     <TodoContext.Provider
