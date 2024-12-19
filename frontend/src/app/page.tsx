e9332e5513bd635b76e15c85e0deffd50e333b38
daichi0918 <kayasugadaichi@icloud.com>
2024-12-19 18:57:06 +0900

    top画面UI実装完了

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,31 +1,11 @@
-'use client';
-
 import { fetchTodoListApi } from '@/apis/todoApi';
 import { setTodoList } from '@/store/todoSlice';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '@/store';
 import { TodoType } from '@/interface/Todo';
+import { TodoTopTemplate } from '@/components/templates/TodoTop';
 
 export default function Home() {
-  const dispatch = useDispatch();
-  const originTodoList = useSelector((state: RootState) => state.todos.lists);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await fetchTodoListApi();
-      dispatch(setTodoList(typeof data === 'object' ? data : [])); // Redux ストアを更新
-    };
-    fetchData();
-  }, [dispatch]);
-  return (
-    <>
-      <div>Home</div>
-      <ul>
-        {originTodoList.map((todo: TodoType) => (
-          <li key={todo?.id}>{todo?.title}</li>
-        ))}
-      </ul>
-    </>
-  );
+  return <TodoTopTemplate />;
 }
---
+++ frontend/src/components/templates/TodoTop/index.tsx
@@ -0,0 +1,62 @@
+'use client';
+/**
+ * TodoTop
+ *
+ * @package templates
+ */
+import styles from './styles.module.css';
+import { EventType } from '@/interface/Event';
+import InputForm from '@/components/atoms/InputForm';
+import { BaseLayout } from '@/components/organisms/BaseLayout';
+import TodoList from '@/components/organisms/TodoList';
+import { useCallback, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '@/store';
+import { fetchTodoListApi } from '@/apis/todoApi';
+import { setTodoList } from '@/store/todoSlice';
+/**
+ * TodoTemplate
+ * @returns {JSX.Element}
+ */
+export const TodoTopTemplate = () => {
+  const [searchKeyWord, setSearchKeyWord] = useState<string>('');
+  const dispatch = useDispatch();
+  const originTodoList = useSelector((state: RootState) => state.todos.lists);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchTodoListApi();
+      dispatch(setTodoList(typeof data === 'object' ? data : [])); // Redux ストアを更新
+    };
+    fetchData();
+  }, [dispatch]);
+
+  /* action定義 */
+  /**
+   * 検索キーワード更新処理
+   * @param {*} e
+   */
+  const handleChangeSearchKeyword: EventType['onChangeInput'] = useCallback(
+    (e) => setSearchKeyWord(e.target.value),
+    []
+  );
+  return (
+    <BaseLayout title={'Todo List'}>
+      <div className={styles.common}>
+        <div className={styles.area}>
+          <InputForm
+            placeholder={'Search Keyword'}
+            value={searchKeyWord}
+            onChange={handleChangeSearchKeyword}
+          />
+        </div>
+        <div className={styles.area}>
+          <TodoList
+            showTodoList={originTodoList}
+            handleDeleteTodoTask={() => console.log('aaa')}
+          />
+        </div>
+      </div>
+    </BaseLayout>
+  );
+};
